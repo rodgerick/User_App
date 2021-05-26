5fe193a723f50540269197a3a84d7f47b3beae50
rodgerick <rodgerick.williams@icloud.com>
2021-05-26 17:23:52 -0600

    first commit

---
+++ db/schema.rb
@@ -0,0 +1,16 @@
+
+ActiveRecord::Schema.define(version: 2021_05_26_211243) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
